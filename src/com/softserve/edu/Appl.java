1da5639a494667f1ea85cf7d25b4377d916334d4
alex85vol <alex5vol@gmail.com>
2016-10-02 21:13:12 +0300

    added new features and fixed answer variable

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -1,5 +1,8 @@
 package com.softserve.edu;
 
+import com.sun.corba.se.spi.extension.ZeroPortPolicy;
+import sun.awt.geom.Crossings;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,59 +11,60 @@ import java.util.List;
 
 public class Appl {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		double a = 0;
-		double b = 0;
-		List<String> figures = new ArrayList<>();
-		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-		// BufferedReader answer = new BufferedReader(new
-		// InputStreamReader(System.in));
-		do {
+        double a = 0;
+        double b = 0;
+        int num = 0;
+        int i = 1;
+        List<String> figures = new ArrayList<>();
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        String answer = null;
+        do {
 
-			try {
-				do {
-					System.out.println(
-							"Please enter name of figure. It should consist of 'square' or 'rectangle' word on the begining: ");
-					figures.add(console.readLine().toUpperCase());
-				} while (figures.size() < 3);
-				System.out.print("Please enter value for a side of figure: ");
-				a = Double.parseDouble(console.readLine());
-				System.out.print("Please enter value for b side of figure: ");
-				b = Double.parseDouble(console.readLine());
-			} catch (Exception e) {
-				System.out.println("Error input");
-			}
-			Rectangle rec = new Rectangle();
-			Square squ = new Square();
+            try {
+                System.out.println("Please enter number of figures (1...9): ");
+                num = Integer.parseInt(console.readLine());
+                do {
+                    System.out.println(
+                            "Please enter name of figure number" + i + ". It should consist of 'square' or 'rectangle' word on the begining: ");
+                    figures.add(console.readLine().toUpperCase());
+                    i++;
+                } while (figures.size() < num);
+                System.out.print("Please enter value for A side of figure: ");
+                a = Double.parseDouble(console.readLine());
+                System.out.print("Please enter value for B side of figure: ");
+                b = Double.parseDouble(console.readLine());
+            } catch (Exception e) {
+                System.out.println("Error input");
+            }
 
-			// String figures[] = { "rectangle", "square", "rectangle" };
+            Rectangle rec = new Rectangle();
+            Square squ = new Square();
 
-			for (String str : figures) {
-				if (str.toLowerCase().startsWith("rectangle")) {
-					System.out.println("Type of figure is " + str.toUpperCase());
-					System.out.println(str.toUpperCase() + " perimeter is: " + rec.perimeter(a, b));
-					System.out.println(str.toUpperCase() + " square is: " + rec.square(a, b));
-				} else if (str.toLowerCase().startsWith("square")) {
-					System.out.println("Type of figure is " + str.toUpperCase());
-					System.out.println(str.toUpperCase() + " perimeter is: " + squ.perimeter(a, b));
-					System.out.println(str.toUpperCase() + " square is: " + squ.square(a, b));
-				}
+            for (String str : figures) {
+                if (str.toLowerCase().startsWith("rectangle")) {
+                    System.out.println("Type of figure is " + str.toUpperCase());
+                    System.out.println(str.toUpperCase() + " perimeter is: " + rec.perimeter(a, b));
+                    System.out.println(str.toUpperCase() + " square is: " + rec.square(a, b));
+                } else if (str.toLowerCase().startsWith("square")) {
+                    System.out.println("Type of figure is " + str.toUpperCase());
+                    System.out.println(str.toUpperCase() + " perimeter is: " + squ.perimeter(a, b));
+                    System.out.println(str.toUpperCase() + " square is: " + squ.square(a, b));
+                }
 
-			}
-			    System.out.println("Do you want to continue with  figures and values: ");
-			try {
-				String answer = console.readLine().toUpperCase();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-
-		}
-			 while(answer.equals("Y"));
-
+            }
+            System.out.println("Do you want to continue with new figures and values: ");
+            try {
+                answer = console.readLine().toUpperCase();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
 
+        }
+        while (answer.equals("Y"));
 
-	}
+
+    }
 }
